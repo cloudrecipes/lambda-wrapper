a1c6f6dd17badb192b74ef7537a3cacd15213d0b
antklim <antklim@gmail.com>
2017-11-12 09:13:15 +1100

    CLI package (WIP)

--- cmd/lambda-wrapper/main.go
+++ cmd/lambda-wrapper/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/cloudrecipes/lambda-wrapper/internal/pkg/cli"
 )
 
 func main() {
-	fmt.Println("Hello, Lambda Wrapper")
 	cli.NewCliApp().Run(os.Args)
 }
--- internal/pkg/cli/cli.go
+++ internal/pkg/cli/cli.go
@@ -12,6 +12,12 @@ const Version string = "0.1.0"
 // Description is the applicaion description.
 const Description string = "Lambda wrapper application. Used to wrap libraries into lambda functions to be run on cloud."
 
+// ShortDescription is the applicaion description which comes in the NAME section.
+const ShortDescription string = ""
+
+// Usage is the applicaion usage text.
+const Usage string = "[global options] command [command options]"
+
 type CliApp struct {
 	App *clihandler.App
 }
@@ -21,14 +27,30 @@ func NewCliApp() *CliApp {
 	app.Name = Name
 	app.Version = Version
 	app.Description = Description
+	app.Usage = ShortDescription
+	app.UsageText = Usage
 	app.Flags = []clihandler.Flag{
 		clihandler.StringFlag{Name: "cloud", Value: "AWS", Usage: "cloud provider name"},
 		clihandler.StringFlag{Name: "engine", Value: "node", Usage: "lambda function engine"},
 		clihandler.StringSliceFlag{Name: "services", Usage: "a list of cloud services to initiate in the wrapper"},
-		clihandler.StringFlag{Name: "source", Value: "npm", Usage: "the source where to find library's code"},
-		clihandler.StringFlag{Name: "name", Usage: "the name of the library in the source"},
+		clihandler.StringFlag{Name: "libsource", Value: "npm", Usage: "the source where to find library's code"},
+		clihandler.StringFlag{Name: "libname", Usage: "the name of the library in the source"},
 		clihandler.StringFlag{Name: "output", Usage: "path to save deployable lambda archive"},
-		clihandler.BoolFlag{Name: "test", Usage: "lag to run library's unit tests before wrapping into lambda package"},
+	}
+
+	app.Commands = []clihandler.Command{
+		{
+			Name:        "testlib",
+			Aliases:     []string{"t"},
+			Usage:       "Runs library unit tests",
+			Description: "This command runs unit tests of the library.",
+		},
+		{
+			Name:        "wraplib",
+			Aliases:     []string{"w"},
+			Usage:       "Wraps library into labmda function",
+			Description: "This command wraps library into the archive to deploy to cloud as a lambda function.",
+		},
 	}
 
 	return &CliApp{App: app}
