8227831db5d3dfe1e63f66d9a044821acd0bbaa7
antklim <antklim@gmail.com>
2017-11-12 23:42:40 +1100

    CLI package (WIP)

--- internal/pkg/cli/cli.go
+++ internal/pkg/cli/cli.go
@@ -1,7 +1,10 @@
 // Package cli contains methods to work with command line arguments.
 package cli
 
-import clihandler "gopkg.in/urfave/cli.v1"
+import (
+	"github.com/cloudrecipes/lambda-wrapper/internal/pkg/options"
+	clihandler "gopkg.in/urfave/cli.v1"
+)
 
 // Name is the applicaion name.
 const Name string = "lambda-wrapper"
@@ -10,58 +13,78 @@ const Name string = "lambda-wrapper"
 const Version string = "0.1.0"
 
 // Description is the applicaion description.
-const Description string = "Lambda wrapper application. Used to wrap libraries into lambda functions to be run on cloud."
+const Description = "Wrap the library into lambda function archive."
 
 // ShortDescription is the applicaion description which comes in the NAME section.
 const ShortDescription string = ""
 
 // Usage is the applicaion usage text.
-const Usage string = "[global options] command [command options]"
+const Usage string = "lambda-wrapper [options]"
 
-type CliApp struct {
-	App *clihandler.App
+// App is a structure for the CLI applicaion.
+type App struct {
+	App     *clihandler.App
+	Options *options.Options
 }
 
-func NewCliApp() *CliApp {
+// NewCliApp creates an instance of CLI applicaion.
+func NewCliApp() *App {
+	opts := &options.Options{}
+
 	app := clihandler.NewApp()
 	app.Name = Name
 	app.Version = Version
 	app.Description = Description
 	app.Usage = ShortDescription
 	app.UsageText = Usage
+
 	app.Flags = []clihandler.Flag{
-		clihandler.StringFlag{Name: "cloud", Value: "AWS", Usage: "cloud provider name"},
-		clihandler.StringFlag{Name: "engine", Value: "node", Usage: "lambda function engine"},
-		clihandler.StringSliceFlag{Name: "services", Usage: "a list of cloud services to initiate in the wrapper"},
-		clihandler.StringFlag{Name: "libsource", Value: "npm", Usage: "the source where to find library's code"},
-		clihandler.StringFlag{Name: "libname", Usage: "the name of the library in the source"},
-		clihandler.StringFlag{Name: "output", Usage: "path to save deployable lambda archive"},
+		clihandler.StringFlag{
+			Name:        "cloud",
+			Value:       "AWS",
+			Usage:       "cloud provider name",
+			Destination: &opts.Cloud,
+		},
+		clihandler.StringFlag{
+			Name:        "engine",
+			Value:       "node",
+			Usage:       "lambda function engine",
+			Destination: &opts.Engine,
+		},
+		clihandler.StringSliceFlag{
+			Name:  "services",
+			Usage: "a list of cloud services to initiate in the wrapper",
+		},
+		clihandler.StringFlag{
+			Name:        "libsource",
+			Value:       "npm",
+			Usage:       "the source where to find library's code",
+			Destination: &opts.LibSource,
+		},
+		clihandler.StringFlag{
+			Name:        "libname",
+			Usage:       "the name of the library in the source",
+			Destination: &opts.LibName,
+		},
+		clihandler.StringFlag{
+			Name:        "output",
+			Usage:       "path to save deployable lambda archive",
+			Destination: &opts.Output,
+		},
+		clihandler.BoolFlag{
+			Name:        "test",
+			Usage:       "flag to run library's unit tests before wrapping into lambda package",
+			Destination: &opts.TestRequired,
+		},
 	}
 
-	app.Commands = []clihandler.Command{
-		{
-			Name:        "testlib",
-			Aliases:     []string{"t"},
-			Usage:       "Runs library unit tests",
-			Description: "This command runs unit tests of the library.",
-		},
-		{
-			Name:        "wraplib",
-			Aliases:     []string{"w"},
-			Usage:       "Wraps library into labmda function",
-			Description: "This command wraps library into the archive to deploy to cloud as a lambda function.",
-		},
+	return &App{
+		App:     app,
+		Options: opts,
 	}
-
-	return &CliApp{App: app}
 }
 
-func (cli *CliApp) Run(args []string) error {
+// Run CLI applicaion.
+func (cli *App) Run(args []string) error {
 	return cli.App.Run(args)
-}
-
-// GetRuntimeFlags returns the list of flags and it's values with which applicaion
-// has been started.
-func (cli *CliApp) GetRuntimeFlags() {
-
 }
