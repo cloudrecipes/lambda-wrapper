5239fc78ea691e74b9effcb3da6269ba770648fc
antklim <antklim@gmail.com>
2017-11-13 00:16:23 +1100

    CLI test cases

--- internal/pkg/cli/cli.go
+++ internal/pkg/cli/cli.go
@@ -12,15 +12,6 @@ const Name string = "lambda-wrapper"
 // Version is the current applicaion version.
 const Version string = "0.1.0"
 
-// Description is the applicaion description.
-const Description = "Wrap the library into lambda function archive."
-
-// ShortDescription is the applicaion description which comes in the NAME section.
-const ShortDescription string = ""
-
-// Usage is the applicaion usage text.
-const Usage string = "lambda-wrapper [options]"
-
 // App is a structure for the CLI applicaion.
 type App struct {
 	App     *clihandler.App
@@ -34,45 +25,52 @@ func NewCliApp() *App {
 	app := clihandler.NewApp()
 	app.Name = Name
 	app.Version = Version
-	app.Description = Description
-	app.Usage = ShortDescription
-	app.UsageText = Usage
+
+	app.CustomAppHelpTemplate = `
+Usage: lambda-wrapper [options]
+
+
+  Options:
+
+		{{range .VisibleFlags}}{{.}}
+		{{end}}
+`
 
 	app.Flags = []clihandler.Flag{
 		clihandler.StringFlag{
-			Name:        "cloud",
+			Name:        "cloud, c",
 			Value:       "AWS",
 			Usage:       "cloud provider name",
 			Destination: &opts.Cloud,
 		},
 		clihandler.StringFlag{
-			Name:        "engine",
+			Name:        "engine, e",
 			Value:       "node",
 			Usage:       "lambda function engine",
 			Destination: &opts.Engine,
 		},
 		clihandler.StringSliceFlag{
-			Name:  "services",
+			Name:  "service, s",
 			Usage: "a list of cloud services to initiate in the wrapper",
 		},
 		clihandler.StringFlag{
-			Name:        "libsource",
+			Name:        "libsource, S",
 			Value:       "npm",
 			Usage:       "the source where to find library's code",
 			Destination: &opts.LibSource,
 		},
 		clihandler.StringFlag{
-			Name:        "libname",
+			Name:        "libname, N",
 			Usage:       "the name of the library in the source",
 			Destination: &opts.LibName,
 		},
 		clihandler.StringFlag{
-			Name:        "output",
+			Name:        "output, o",
 			Usage:       "path to save deployable lambda archive",
 			Destination: &opts.Output,
 		},
 		clihandler.BoolFlag{
-			Name:        "test",
+			Name:        "test, t",
 			Usage:       "flag to run library's unit tests before wrapping into lambda package",
 			Destination: &opts.TestRequired,
 		},
--- internal/pkg/cli/cli_test.go
+++ internal/pkg/cli/cli_test.go
@@ -1,1 +1,36 @@
 package cli_test
+
+import (
+	"testing"
+
+	"github.com/cloudrecipes/lambda-wrapper/internal/pkg/cli"
+)
+
+func TestNewCliApp(t *testing.T) {
+	testApp := cli.NewCliApp()
+	if testApp == nil {
+		t.Fatal("Expected Application to be not nil")
+	}
+
+	if testApp.App == nil {
+		t.Fatal("Expected Cli Application handler to be not nil")
+	}
+
+	if testApp.Options == nil {
+		t.Fatal("Expected Application Options to be not nil")
+	}
+}
+
+func TestRun(t *testing.T) {
+	testApp := cli.NewCliApp()
+	args := []string{"lambda-wrapper", "--help"}
+	err := testApp.Run(args)
+
+	// TODO: Add check of app options based on arguments
+	// TODO: separately check help message
+	// TODO: separately check version message
+
+	if err != nil {
+		t.Fatal("Expected Application to be successfully run")
+	}
+}
