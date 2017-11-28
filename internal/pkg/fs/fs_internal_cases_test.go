2f80203ec8f8ef33b43cd6628c64c8539d550c09
antklim <antklim@gmail.com>
2017-11-28 17:05:59 +1100

    More coverage for ZIP tests

--- internal/pkg/fs/fs_internal_cases_test.go
+++ internal/pkg/fs/fs_internal_cases_test.go
@@ -5,18 +5,49 @@ import (
 	"errors"
 	"io"
 	"os"
+	"time"
 )
 
 type BrokenZipWriter struct{}
 
 func (w *BrokenZipWriter) CreateHeader(*zip.FileHeader) (io.Writer, error) {
-	return nil, errors.New("Create Header")
+	return nil, errors.New("Create Header Error")
 }
 
 // make sure it satisfies the interface
 var _ zipWriter = (*BrokenZipWriter)(nil)
 
-var brokenWriter = &BrokenZipWriter{}
+var brokenZipWriter = &BrokenZipWriter{}
+
+type TestFileInfo struct{}
+
+func (i *TestFileInfo) Name() string {
+	return ""
+}
+
+func (i *TestFileInfo) IsDir() bool {
+	return false
+}
+
+func (i *TestFileInfo) Mode() os.FileMode {
+	return os.ModePerm
+}
+
+func (i *TestFileInfo) ModTime() time.Time {
+	return time.Now()
+}
+
+func (i *TestFileInfo) Size() int64 {
+	return 0
+}
+
+func (i *TestFileInfo) Sys() interface{} {
+	return nil
+}
+
+var _ os.FileInfo = (*TestFileInfo)(nil)
+
+var testFileInfo = &TestFileInfo{}
 
 var filepathWalkTestCases = []struct {
 	basedir  string
@@ -28,5 +59,5 @@ var filepathWalkTestCases = []struct {
 	expected error
 }{
 	{basedir: "", source: "", archive: nil, path: "", info: nil, err: errors.New("Test Error"), expected: errors.New("Test Error")},
-	// {basedir: "", source: "", archive: &BrokenWriter{}, path: "blah", info: nil, err: nil, expected: errors.New("Test Error")},
+	{basedir: "", source: "", archive: brokenZipWriter, path: "blah", info: testFileInfo, err: nil, expected: errors.New("Create Header Error")},
 }
