f70c19f2bf9ac0588f1430fcc4c3e26fd55f3dfc
antklim <antklim@gmail.com>
2018-01-07 15:18:50 +1100

    added validation of npm command on a host OS

--- internal/pkg/sourcer/sourcer.go
+++ internal/pkg/sourcer/sourcer.go
@@ -8,6 +8,7 @@ type Sourcer interface {
 	LibGet(c cmd.Commander, libname, workingdir string) ([]byte, error)
 	LibTest(c cmd.Commander, workingdir string) ([]byte, error)
 	LibDeps(c cmd.Commander, workingdir string, isprod bool) ([]byte, error)
+	VerifySourcerCommands(c cmd.Commander) error
 }
 
 // LibGet downloads/gets library using different source types.
@@ -23,6 +24,12 @@ func LibTest(s Sourcer, workingdir string) ([]byte, error) {
 // LibDeps downloads librarie's dependencies.
 func LibDeps(s Sourcer, workingdir string, isprod bool) ([]byte, error) {
 	return s.LibDeps(&cmd.RealCommander{}, workingdir, isprod)
+}
+
+// VerifySourcerCommands checks if all the necessary tools are available on the
+// host OS.
+func VerifySourcerCommands(s Sourcer) error {
+	return s.VerifySourcerCommands(&cmd.RealCommander{})
 }
 
 // TODO: Check if host OS has installed git and npm
