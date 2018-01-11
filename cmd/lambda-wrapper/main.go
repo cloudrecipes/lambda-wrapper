618865ffb106071d8ebadce53e4c6678cb211d19
antklim <antklim@gmail.com>
2018-01-11 15:47:29 +1100

    lambda template updated, commands numbers updated

--- cmd/lambda-wrapper/main.go
+++ cmd/lambda-wrapper/main.go
@@ -59,85 +59,83 @@ func main() {
 			return err
 		}
 
-		fmt.Println("[1.1] verifying all required tools are installed on a host OS...")
+		fmt.Println("[2] verifying all required tools are installed on a host OS...")
 		if err = sourcer.VerifySourcerCommands(commander); err != nil {
-			fmt.Printf("[1.1] sourcer commands verification failed: %v\n", err)
+			fmt.Printf("[2] sourcer commands verification failed: %v\n", err)
 			return err
 		}
 
 		// Create `.lwtmp`, `.lwtmp/lib`, `.lwtmp/build` directories in Output directory
-		fmt.Println("[2] making dirs...")
+		fmt.Println("[3] making dirs...")
 		if err = f.MakeDirs(opts.Output); err != nil {
-			fmt.Printf("[2] make dirs: %v\n", err)
+			fmt.Printf("[3] make dirs: %v\n", err)
 			return err
 		}
 
 		// Install library into `.lwtmp/lib`
-		fmt.Println("[3] getting library...")
+		fmt.Println("[4] getting library...")
 		workingdir = path.Join(opts.Output, f.LibDir())
 		if _, err = sourcer.LibGet(commander, opts.LibName, workingdir); err != nil {
-			fmt.Printf("[3] library get: %v\n", err)
+			fmt.Printf("[4] library get: %v\n", err)
 			return err
 		}
 
 		// Install dependencies to `.lwtmp/lib`
-		fmt.Println("[4] installing library dependencies...")
+		fmt.Println("[5] installing library dependencies...")
 		if _, err = sourcer.LibDeps(commander, workingdir, false); err != nil {
-			fmt.Printf("[4] library dependencies: %v\n", err)
+			fmt.Printf("[5] library dependencies: %v\n", err)
 			return err
 		}
 
 		// Run unit tests at `.lwtmp/lib`
 		if opts.TestRequired {
-			fmt.Println("[5] running library tests...")
+			fmt.Println("[6] running library tests...")
 			if _, err = sourcer.LibTest(commander, workingdir); err != nil {
-				fmt.Printf("[5] library test: %v\n", err)
+				fmt.Printf("[6] library test: %v\n", err)
 				return err
 			}
 		}
 
 		// Install library into `.lwtmp/build`
-		fmt.Println("[6] getting library for build...")
+		fmt.Println("[7] getting library for build...")
 		workingdir = path.Join(opts.Output, f.BuildDir())
 		if _, err = sourcer.LibGet(commander, opts.LibName, workingdir); err != nil {
-			fmt.Printf("[6] build library get: %v\n", err)
+			fmt.Printf("[7] build library get: %v\n", err)
 			return err
 		}
 
 		// Install production dependencies to `.lwtmp/build`
-		fmt.Println("[7] installing library dependencies...")
+		fmt.Println("[8] installing library dependencies...")
 		if _, err = sourcer.LibDeps(commander, workingdir, true); err != nil {
-			fmt.Printf("[7] build library dependencies: %v\n", err)
+			fmt.Printf("[8] build library dependencies: %v\n", err)
 			return err
 		}
 
 		// Prepare lambda wrapper
-		fmt.Println("[8] geting wrapper...")
+		fmt.Println("[9] geting wrapper...")
 		if wrapper, err = getWrapper(opts.Cloud); err != nil {
-			fmt.Printf("[8] get wrapper: %v\n", err)
+			fmt.Printf("[9] get wrapper: %v\n", err)
 			return err
 		}
 
 		// Wrap code into lambda
-		fmt.Println("[9] wrapping...")
+		fmt.Println("[10] wrapping...")
 		if lambda, err = w.Wrap(wrapper, opts, w.DefaultTemplateDir()); err != nil {
-			fmt.Printf("[9] wrap: %v\n", err)
+			fmt.Printf("[10] wrap: %v\n", err)
 			return err
 		}
 
-		// TODO: this code does not belong to main.go.
-		// Move it to either wrapper or fs package.
-		fmt.Println("[10] writing lambda handler...")
+		fmt.Println("[11] writing lambda handler...")
 		filename := path.Join(workingdir, "index.js")
 		if err = w.Save(filename, lambda, fs); err != nil {
-			fmt.Printf("[10] write wrapper: %v\n", err)
+			fmt.Printf("[11] write wrapper: %v\n", err)
 			return err
 		}
 
 		// Zip package for deploy
-		fmt.Println("[11] save deployables...")
+		fmt.Println("[12] save deployables...")
 		if err := fs.ZipDir(workingdir, "tmp.zip"); err != nil {
-			fmt.Printf("[11] save deployables: %v\n", err)
+			fmt.Printf("[12] save deployables: %v\n", err)
 			return err
 		}
 
